b2c40418f7989be645edb186540e03d0f860dc05
MrDevTheDev <73756572+MrDevTheDev@users.noreply.github.com>
2021-07-31 17:07:08 -0500

    Delete fun directory

--- fun/say.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: "say",
-  description: "Make the bot say something",
-  execute(message, args) {
-    return message.channel.send(message.content.replace("p~say ", ""));
-  }
-}
